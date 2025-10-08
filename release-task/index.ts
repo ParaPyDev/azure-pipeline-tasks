7c1db6060447ac733a232a64ec1b71ad14cac567
ParaPyDev <dev@parapy.nl>
2025-10-08 09:11:50 +0200

    feat: update release task to use the new CLI, and add option to specify ParaPy Cloud version (#4)

--- release-task/index.ts
+++ release-task/index.ts
@@ -15,12 +15,25 @@ const { exec } = require("child_process");
     parapyCloudCLIVersion = parapyCloudCLIVersion ? "~=" + parapyCloudCLIVersion : ""
     // this task assumes the ParaPy application code is already cloned and resides in the current folder
     // and that python is installed on the machine (for example with the UsePythonVersion task: https://learn.microsoft.com/en-us/azure/devops/pipelines/tasks/reference/use-python-version-v0)
+    let parapyCloudVersion: string | undefined = tl.getInput('parapyCloudVersion', false);
 
     await runCommandsOrThrow([
-            'pip install --upgrade pip --no-input',
+        'pip install --upgrade pip --no-input',
+    ]);
+
+    // special case, if Cloud version is 0.X.Y, then use the old tool to deploy
+    if (parapyCloudVersion && parseInt(parapyCloudVersion.split(".")[0]) < 1) {
+        await runCommandsOrThrow([
             `pip install parapy-cloud-cli${parapyCloudCLIVersion} --index-url https://${ parapyPyPIUsername }:${ parapyPyPIPassword }@${ parapyPyPIAddress }/simple/ --no-input`,
             `parapy cloud app release . --url ${ parapyCloudAddress } --client-id ${ serviceAccountIdentifier } --secret ${ serviceAccountSecret } --version ${ parapyAppVersion } --id ${ parapyAppIdentifier } ${deploy ? " --deploy": "" }`
         ]);
+        return
+    }
+    // normally use the new tool
+    await runCommandsOrThrow([
+        `pip install parapy-cli --index-url https://${ parapyPyPIUsername }:${ parapyPyPIPassword }@${ parapyPyPIAddress }/simple/ --no-input`,
+        `parapy app release . --url ${ parapyCloudAddress } -c ${ serviceAccountIdentifier } -s ${ serviceAccountSecret } -v ${ parapyAppVersion } --id ${ parapyAppIdentifier }`
+    ]);
  }
 
  function runCommandPromise(command: string): Promise<void> {
